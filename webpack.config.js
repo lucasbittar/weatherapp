4a07f78da993eddaeb62b9d3fa4df2fbc4c26f01
Lucas Bittar Magnani <lucasbittar.magnani@gmail.com>
2017-02-01 00:54:21 -0200

    Add webpack config file and update package.json file with webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
+/**
+ * Project Name: Weather App | Webpack Config
+ * Author: Lucas Bittar Magnani
+ * Created: 20170131
+ */
+
 module.exports = {
   context: __dirname + '/app',
-  entry: './main.js',
+  entry: './scripts/main.js',
   output: {
     path: __dirname + '/app',
     filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: 'url-loader?limit=10000!img-loader?progressive=true'
+      }
+    ]
   }
 }
